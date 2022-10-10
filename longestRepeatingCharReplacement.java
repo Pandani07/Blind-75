57ee39028c2b0801e5c88d08b37db8be11e4df7e
Anirudh Dutt <anidutt27@gmail.com>
2022-10-09 20:53:30 -0400

    jkfeebf

---
+++ longestRepeatingCharReplacement.java
@@ -0,0 +1,21 @@
+public class longestRepeatingCharReplacement {
+    public int solution(String s, int k){
+        int res=0;
+        int i=0,max=0;
+
+        int[] arr = new int[26];
+
+        for(int j=0;j<s.length();j++){
+            arr[s.charAt(j)-'A']++;
+            max = Math.max(max, arr[s.charAt(j)-'A']);
+
+            if(j-i+1-max>k){
+                arr[s.charAt(i)-'A']--;
+                i++;
+            }
+
+            res = Math.max(res, j-i+1);
+        }
+        return res;
+    }
+}
